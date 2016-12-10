59a66e68db58bbbfdef20334c0d726f4bd291330
croef <changrong185@gmail.com>
2016-12-10 21:44:54 +0800

    update init shell

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [ "$1" == 'init' ]; then
+if [ "$1" = 'init' ]; then
     hexo init
-	npm install
-elif [ "$1" == "bash" ]; then
+    npm install 
+elif [ "$1" = 'bash' ]; then
 	/bin/bash
 else
-	echo "$*"
 	hexo "$*"
 fi
