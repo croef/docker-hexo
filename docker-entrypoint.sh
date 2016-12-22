8fa64fb4d0530c7201b2f018e2836eddb079af9c
croef <changrong185@gmail.com>
2016-12-22 21:51:49 +0800

    add custom update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 if [ "$1" = 'init' ]; then
+    cd /root/blog
     hexo init
     npm install 
 elif [ "$1" = 'bash' ]; then
 	/bin/bash
+elif [ "$1" = 'pull' ]; then
+    cd /root/blog/
+    /bin/bash custom-update.sh
 else
+    cd /root/blog
 	hexo "$*"
 fi
